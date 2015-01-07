7357489f069856314ba7da2d20fc879ae881ca9e
kaiyiz <kaiyiz@codeaurora.org>
2015-01-07 15:24:05 +0800

    SnapdragonCamera: Quick icon disappear after stop record by home key
    Color effect and switch camera icons disappear if re-launch camera after
    press home key during recording video. The icons are hided when start
    record video, but show icons aren't called after stop video record by
    press home key.
    
    Show and hide icons should always be called when stop and start record
    video, so move the functions to stopVideoRecord and startVideoRecord.
    
    Change-Id: Ie4fe9102e226452502fcc382329cc0e01799b0cf
    
    CRs-Fixed: 777114

--- src/com/android/camera/VideoModule.java
+++ src/com/android/camera/VideoModule.java
@@ -1818,6 +1818,7 @@ public class VideoModule implements CameraModule,
         mStartRecPending = true;
         mUI.cancelAnimations();
         mUI.setSwipingEnabled(false);
+        mUI.hideUIwhileRecording();
 
         mActivity.updateStorageSpaceAndHint();
         if (mActivity.getStorageSpaceBytes() <= Storage.LOW_STORAGE_THRESHOLD_BYTES) {
